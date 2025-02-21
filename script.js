2abb72344144f8019558cfda20329dc87403e29c
ninh <ntninh00@gmail.com>
2025-02-21 19:34:38 +0700

    temp fix

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js';
 import { getDatabase, ref, set, onValue } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js';
-import { firebaseConfig } from './firebase-config.js'; 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAotuyVf5GV2f-w2_9nR3VsJ_LHbtX4xsM",
+    authDomain: "kanji-flashcard.firebaseapp.com",
+    projectId: "kanji-flashcard",
+    storageBucket: "kanji-flashcard.firebasestorage.app",
+    messagingSenderId: "158390700713",
+    appId: "1:158390700713:web:e255acedd26a26d3c177b8",
+    measurementId: "G-MRE2WC4Y7Q",
+    databaseURL: "https://kanji-flashcard-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
